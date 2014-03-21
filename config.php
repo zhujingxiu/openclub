67733d4217a2f9a041b7ad8ea1e6a4b395b078a6
zhujingxiu <328053730@qq.com>
2014-03-21 14:36:29 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,27 +1,15 @@
 <?php
-// HTTP
-define('HTTP_SERVER', 'http://www.club.com/');
-define('HTTP_CATALOG', 'http://www.club.com/');
-
-// HTTPS
-define('HTTPS_SERVER', 'http://www.club.com/');
-define('HTTPS_CATALOG', 'http://www.club.com/');
-
-//Document Root
-//define('DOCUMENTROOT','/var/www/');
-define('DOCUMENTROOT','E:\wamp\www\club/');
 // DIR
-define('DIR_APPLICATION', DOCUMENTROOT.'protected/');
-define('DIR_SYSTEM', DOCUMENTROOT.'system/');
-define('DIR_DATABASE', DOCUMENTROOT.'system/database/');
-define('DIR_LANGUAGE', DOCUMENTROOT.'protected/language/');
-define('DIR_TEMPLATE', DOCUMENTROOT.'protected/view/template/');
-define('DIR_CONFIG', DOCUMENTROOT.'system/config/');
-define('DIR_IMAGE', DOCUMENTROOT.'image/');
-define('DIR_CACHE', DOCUMENTROOT.'system/cache/');
-define('DIR_DOWNLOAD', DOCUMENTROOT.'download/');
-define('DIR_LOGS', DOCUMENTROOT.'system/logs/');
-define('DIR_CATALOG', DOCUMENTROOT.'catalog/');
 
+define('DIR_APPLICATION', APP_PATH.'/application/');
+define('DIR_SYSTEM', APP_PATH.'/system/');
+define('DIR_DATABASE', APP_PATH.'/system/database/');
+define('DIR_LANGUAGE', APP_PATH.'/application/language/');
+define('DIR_TEMPLATE', APP_PATH.'/application/view/template/');
+define('DIR_CONFIG', APP_PATH.'/system/config/');
+define('DIR_IMAGE', APP_PATH.'/image/');
+define('DIR_CACHE', APP_PATH.'/system/cache/');
+define('DIR_DOWNLOAD', APP_PATH.'/download/');
+define('DIR_LOGS', APP_PATH.'/system/logs/');
 
 ?>
