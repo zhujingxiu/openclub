ed6bcfc08f80f1556e2949f0e4ca85d71e80e0d0
zhujingxiu <328053730@qq.com>
2014-03-21 14:42:43 +0800

    Update config.php

--- system/library/config.php
+++ system/library/config.php
@@ -14,16 +14,26 @@ class Config {
     	return isset($this->data[$key]);
   	}
 
-  	public function load($filename) {
+  	public function load($filename,$define=False) {
 		$file = DIR_CONFIG . $filename . '.php';
 		
     	if (file_exists($file)) { 
 	  		$_ = array();
 	  
 	  		require($file);
-	  
-	  		$this->data = array_merge($this->data, $_);
+	  		
+    			if($define){
+	  			foreach ($_ as $name => $value){
+	  				if( is_string($value)){
+	  					defined(strtoupper(trim($name)))  || define(strtoupper(trim($name)),  $value );
+	  				}
+	  			}
+	  		}else{
+	  			$this->data = array_merge($this->data, $_);
+	  		}
+	  		
 		} else {
+			echo $file;
 			trigger_error('Error: Could not load config ' . $filename . '!');
 			exit();
 		}
