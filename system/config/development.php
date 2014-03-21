d2a206e5ba714d68e8c932fbe7e91ceded375c52
zhujingxiu <328053730@qq.com>
2014-03-21 14:31:48 +0800

    Create development.php

---
+++ system/config/development.php
@@ -0,0 +1,20 @@
+<?php
+// Development Environment
+// HTTP
+$_['HTTP_SERVER'] 		= 'http://www.club.com/';
+$_['HTTP_CATALOG'] 		= 'http://www.club.com/';
+
+// HTTPS
+$_['HTTPS_SERVER'] 		= 'http://www.club.com/';
+$_['HTTPS_CATALOG'] 	= 'http://www.club.com/';
+
+
+// DB
+$_['DB_DRIVER'] 		= 'mysqli';
+$_['DB_HOSTNAME'] 		= '192.168.1.123';
+$_['DB_USERNAME'] 		= 'lively';
+$_['DB_PASSWORD'] 		= 'jingjian';
+$_['DB_DATABASE'] 		= 'mmc';
+$_['DB_PREFIX'] 		= 'mmc_';
+
+?>
